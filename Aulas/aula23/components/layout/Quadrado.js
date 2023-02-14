a161ffcf28d3a180682eba4e6233c977a466746c
Gustavo Ferreira <gfmb94@gmail.com>
2023-02-13 23:49:15 -0300

    Usando Flexbox #01

---
+++ Aulas/aula23/components/layout/Quadrado.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export default props => {
+  const lado = 35
+  return (
+    <View style={{
+      height: lado,
+      width: lado,
+      backgroundColor: props.cor || '#000'
+    }}/>
+  )
+}
